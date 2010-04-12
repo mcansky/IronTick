9e7be1675bf1d8fab1f4aa8b3faa7a83f1d7fbe1
T Riboulet <riboulet@gmail.com>
2010-04-12 17:23:48 +0200

    adding ticket associations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,6 @@
 class Ticket < ActiveRecord::Base
   attr_accessible :title, :status, :text, :owner_id, :project_id, :customer_id, :created_at
+  belongs_to :manager, :class_name => "User", :foreign_key => "owner_id"
+  belongs_to :customer, :class_name => "User", :foreign_key => "customer_id"
+  belongs_to :project
 end
