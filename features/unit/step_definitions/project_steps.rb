1eba65302810bb928868a046254bfb718e7f05e3
T Riboulet <riboulet@gmail.com>
2010-04-12 16:33:55 +0200

    defining steps for project

--- features/unit/step_definitions/project_steps.rb
+++ features/unit/step_definitions/project_steps.rb
@@ -1,19 +1,19 @@
 Given /^a project$/ do
-  pending # express the regexp above with the code you wish you had
+  @project = Factory.build(:project)
 end
 
 When /^we load it$/ do
-  pending
+  @project.should_not == nil
 end
 
 Then /^it should have a description$/ do
-  pending # express the regexp above with the code you wish you had
+  @project.description.should_not == nil
 end
 
 Then /^it should have tickets$/ do
-  pending # express the regexp above with the code you wish you had
+  @project.tickets.should_not == nil
 end
 
 Then /^it should belongs to a company$/ do
-  pending # express the regexp above with the code you wish you had
+  @project.company.should_not == nil
 end
