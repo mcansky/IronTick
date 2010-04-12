498d1e80e40f122b7e2d02b53e95681922ce7ddf
T Riboulet <riboulet@gmail.com>
2010-04-12 17:33:53 +0200

    adding user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :login, :email, :phone, :company_id, :firstname, :lastname
+  belongs_to :company
+  has_many :managed_tickets, :class_name => "Ticket", :foreign_key => "owner_id"
+  has_many :owned_tickets, :class_name => "Ticket", :foreign_key => "customer_id"
 end
