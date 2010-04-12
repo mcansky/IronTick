e10776dcbd1c98851d454639cbcab96e3ed72c65
T Riboulet <riboulet@gmail.com>
2010-04-12 16:42:30 +0200

    passing the migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100412141524) do
+ActiveRecord::Schema.define(:version => 20100412143558) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
     t.text     "description"
     t.text     "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "company_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
