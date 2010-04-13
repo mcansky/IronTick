00d361fd2b05ef17925b7b6611a7ce4ac6971e07
T Riboulet <riboulet@gmail.com>
2010-04-13 14:18:06 +0200

    adding basic steps definition

---
+++ features/customer/step_definitions/homepage_steps.rb
@@ -0,0 +1,11 @@
+Given /^a customer$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^he logs in$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^he should see a list of projects$/ do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/customer/step_definitions/project_steps.rb
@@ -0,0 +1,27 @@
+Given /^a project and customer$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the customer logs in$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^he check a project$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^he should see the project title$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^he should see the project description$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^he should see last 10 tickets name$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^he should see last 10 tickets status$/ do
+  pending # express the regexp above with the code you wish you had
+end
