b2bfc5c0b79482f1f6b42ec0e78ba06a0e6561eb
T Riboulet <riboulet@gmail.com>
2010-04-13 20:30:10 +0200

    adding login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :projects
+  map.resources :user_sessions, :projects
+  
+	map.login "login", :controller => "user_sessions", :action => "new"
+	map.logout "logout", :controller => "user_sessions", :action => "destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
 
