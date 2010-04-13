e7de059d6e75379b574bf62728c5809d4ff43341
T Riboulet <riboulet@gmail.com>
2010-04-13 21:30:01 +0200

    fixing the steps and feature definition

--- features/admin/project.feature
+++ features/admin/project.feature
@@ -1,11 +1,11 @@
-In order for a customer to check basic info about the project
+In order for an admin to check basic info about the project
 
-Scenario: the customer logs in, check a project
+Scenario: the admin logs in, check a project
 	Given a project and admin
 	When the admin logs in
-	And he check a project
-	Then he should see the project title within "h1"
-	And he should see the project description within "p"
-	And he should see "Last 10 tickets" within "h2"
-	Then he should see "Edit" within "a"
-	And he should see "Destroy" within "a"
+	And the admin check a project
+	Then the admin should see the project title within "h1"
+	And the admin should see the project description within "p"
+	And the admin should see "Last 10 tickets" within "h2"
+	Then the admin should see "Edit" within "a"
+	And the admin should see "Destroy" within "a"
--- features/admin/step_definitions/project_steps.rb
+++ features/admin/step_definitions/project_steps.rb
@@ -1,32 +1,29 @@
-Given /^a project and customer$/ do
+Given /^a project and admin$/ do
   @project = Factory.create(:project)
   @admin = Factory.create(:admin)
 end
 
 When /^the admin logs in$/ do
   visit login_path
-  fill_in "login", :with => @customer.login
-  fill_in "password", :with => @customer.password
+  fill_in "login", :with => @admin.login
+  fill_in "password", :with => @admin.password
   click_button "Login"
 end
 
-When /^he check a project$/ do
+When /^the admin check a project$/ do
   visit url_for(@project)
 end
 
-Then /^he should see the project title within "([^\"]*)"$/ do |arg1|
+Then /^the admin should see the project title within "([^\"]*)"$/ do |arg1|
   response.should contain(@project.name)
 end
 
-Then /^he should see the project description within "([^\"]*)"$/ do |arg1|
+Then /^the admin should see the project description within "([^\"]*)"$/ do |arg1|
   response.should contain(@project.description)
 end
 
-Then /^he should see "([^\"]*)" within "([^\"]*)"$/ do |arg1, arg2|
+Then /^the admin should see "([^\"]*)" within "([^\"]*)"$/ do |arg1, arg2|
   response.should contain(arg1)
 end
 
-Then /^he should see "([^\"]*)" within "([^\"]*)"$/ do |arg1, arg2|
-  response.should_not contain(arg1)
-end
 
