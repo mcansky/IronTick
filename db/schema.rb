ae65b9561c0563c4c28604f8004027e22969fa45
T Riboulet <riboulet@gmail.com>
2010-04-13 16:18:02 +0200

    adding role attribute in db

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100412143558) do
+ActiveRecord::Schema.define(:version => 20100413141529) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(:version => 20100412143558) do
     t.integer  "company_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
 end
